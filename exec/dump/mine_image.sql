891ad4205818878c7d93dc34dced0f774f672995
김민건 <mine702@naver.com>
2024-05-19 03:11:56 +0900

    fix exec
    Former-commit-id: 8125ebfd59da97b361193d602a317c645f04e276 [formerly 1e979b92f23db9dc2eb71d78895eb40f0a8304ea] [formerly 459a9ff3cb26f0a636cd359979cacf526998eab7 [formerly e4d34238988e2c93db8c323c13ee34fde3cbe8ad]]
    Former-commit-id: 19e7c6d7dfff49dad86f3582df87b4cc80578a85 [formerly b4e8422150b228e9524747d91632f4ee0feef79a]
    Former-commit-id: 05b7d427cd4947d967493041fbfd163d1f1058d7

--- exec/dump/mine_image.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5886857540c9104553a8f5120ca15d46ec4e2e48e8748b913fbfb234ffb32792
-size 12076
--- exec/dump/mine_member.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:36673459b83f719d7d1ad2187906744d6c4fedc5d26804be813a824e2d5c3641
-size 5214
